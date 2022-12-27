b8f26c67607d6e25bb78c5f00001db724c72f587
DESKTOP-G03N79N\csaba.szasz <cs.szasz@gmail.com>
2022-12-27 21:03:11 +0200

    bottom navigation added

--- 3Track/app/src/main/java/com/example/a3track/MainActivity.kt
+++ 3Track/app/src/main/java/com/example/a3track/MainActivity.kt
@@ -3,14 +3,48 @@ package com.example.a3track
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.a3track.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("TAG", "MainActivity - onCreate()")
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-//        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-//        setContentView(binding.root)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        navController = findNavController(R.id.nav_host_fragment)
+
+        hideNavBars()
+
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+    private fun hideNavBars() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.loginFragment -> {
+                    bottomNavigationView.visibility = View.GONE
+                }
+                R.id.taskDetailFragment -> {
+                    bottomNavigationView.visibility = View.GONE
+                }
+                else -> {
+                    bottomNavigationView.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }

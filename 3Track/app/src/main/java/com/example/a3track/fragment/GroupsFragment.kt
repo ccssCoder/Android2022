794acc0b30b1e872b282882c94333ebe8baaa22a
DESKTOP-G03N79N\csaba.szasz <cs.szasz@gmail.com>
2023-01-07 15:16:00 +0200

    bug fix: keyboard was hiding input fields on form

--- 3Track/app/src/main/java/com/example/a3track/fragment/GroupsFragment.kt
+++ 3Track/app/src/main/java/com/example/a3track/fragment/GroupsFragment.kt
@@ -5,12 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.a3track.R
+import com.example.a3track.api.ThreeTrackerRepository
+import com.example.a3track.viewmodel.AddTaskViewModel
+import com.example.a3track.viewmodel.AddTaskViewModelFactory
+import com.example.a3track.viewmodel.GroupsViewModel
+import com.example.a3track.viewmodel.GroupsViewModelFactory
 
 class GroupsFragment : Fragment() {
+    private lateinit var groupsViewModel: GroupsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val factory = GroupsViewModelFactory(ThreeTrackerRepository())
+        groupsViewModel = ViewModelProvider(this, factory)[GroupsViewModel::class.java]
     }
 
     override fun onCreateView(
---
+++ 3Track/app/src/main/java/com/example/a3track/viewmodel/GroupsViewModel.kt
@@ -0,0 +1,71 @@
+package com.example.a3track.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.a3track.App
+import com.example.a3track.api.ThreeTrackerRepository
+import com.example.a3track.api.model.DepartmentResponse
+import com.example.a3track.api.model.UserResponse
+import com.example.a3track.manager.SharedPreferencesManager
+import kotlinx.coroutines.launch
+
+class GroupsViewModel(private val repository: ThreeTrackerRepository) : ViewModel()  {
+    var departments: MutableLiveData<List<DepartmentResponse>?> = MutableLiveData()
+    var members: MutableLiveData<List<UserResponse>?> = MutableLiveData()
+
+    fun getDepartments(){
+        viewModelScope.launch {
+            try {
+                val token: String? = App.sharedPreferences.getStringValue(
+                    SharedPreferencesManager.KEY_TOKEN,
+                    "Empty token!"
+                )
+                val response = token?.let {
+                    repository.getDepartments(it)
+                }
+
+                if (response?.isSuccessful == true) {
+                    Log.d("TAG", "Get departments response: ${response.body()}")
+
+                    val departmentList = response.body()
+                    departmentList?.let {
+                        departments.value = departmentList
+                    }
+                } else {
+                    Log.d("TAG", "Get departments error response: ${response?.errorBody()}")
+                }
+
+            } catch (e: Exception) {
+                Log.d("TAG", "AddTasksViewModel - getDepartments() failed with exception: ${e.message}")
+            }
+        }
+    }
+
+    fun getMembers(){
+        viewModelScope.launch {
+            try {
+                val token: String? = App.sharedPreferences.getStringValue(
+                    SharedPreferencesManager.KEY_TOKEN,
+                    "Empty token!"
+                )
+                val response = token?.let {
+                    repository.getUsers(it)
+                }
+                if (response?.isSuccessful == true) {
+                    Log.d("TAG", "Get departments response: ${response.body()}")
+                    val membersList = response.body()
+                    membersList?.let {
+                        members.value = membersList
+                    }
+                } else {
+                    Log.d("TAG", "Get departments error response: ${response?.errorBody()}")
+                }
+
+            } catch (e: Exception) {
+                Log.d("TAG", "AddTasksViewModel - getDepartments() failed with exception: ${e.message}")
+            }
+        }
+    }
+}

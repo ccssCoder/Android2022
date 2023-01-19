d00563f903115857cbd1dfe3bc01c2d48f48a29f
DESKTOP-G03N79N\csaba.szasz <cs.szasz@gmail.com>
2023-01-19 15:00:51 +0200

    started working on toolbar

--- 3Track/app/src/main/java/com/example/a3track/MainActivity.kt
+++ 3Track/app/src/main/java/com/example/a3track/MainActivity.kt
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.a3track.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,6 +16,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var bottomNavigationView: BottomNavigationView
+    private lateinit var toolbar: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d("TAG", "MainActivity - onCreate()")
@@ -27,21 +31,41 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView = findViewById(R.id.bottom_navigation)
         navController = findNavController(R.id.nav_host_fragment)
 
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.tasksFragment,
+                R.id.groupsFragment,
+                R.id.settingsFragment,
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
         hideNavBars()
 
         bottomNavigationView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
     private fun hideNavBars() {
         navController.addOnDestinationChangedListener { _, destination, _ ->
             when (destination.id) {
                 R.id.loginFragment -> {
+                    toolbar.visibility = View.GONE
                     bottomNavigationView.visibility = View.GONE
                 }
                 R.id.taskDetailFragment -> {
                     bottomNavigationView.visibility = View.GONE
                 }
                 else -> {
+                    toolbar.visibility = View.VISIBLE
                     bottomNavigationView.visibility = View.VISIBLE
                 }
             }
